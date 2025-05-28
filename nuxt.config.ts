f7d5ae6ae92a3a5eb180272e88bc6d28dabb8f3c
Gustavo Barragan <dev@droni.co>
2025-05-27 20:50:28 -0500

    fix: tailwind version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: [],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
